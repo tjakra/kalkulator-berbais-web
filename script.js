61bce9ebfb6ba83134171fc19fe1785e3fc4dcb3
tjakra <cakrasatria01@gmail.com>
2023-07-04 12:23:37 +0700

    kalkulator

---
+++ script.js
@@ -0,0 +1,73 @@
+
+function sec(nomer) {
+    input = document.getElementById("total").innerHTML;
+    input = input + nomer;
+    document.getElementById("total").innerHTML = input;
+
+}
+
+function mines() {
+    input = document.getElementById("total").innerHTML;
+    input = "-" + input;
+    document.getElementById("total").innerHTML = input;
+}
+
+function pangkat() {
+    input = document.getElementById("total").innerHTML;
+    input = input * 1 / 100;
+    document.getElementById("total").innerHTML = input;
+}
+
+function note() {
+    input = document.getElementById("total").innerHTML
+    input = input + "."
+    document.getElementById("total").innerHTML = input;
+}
+
+function hapus(){
+    document.getElementById("total").innerHTML = ""
+}
+
+function sub() {
+    input = document.getElementById("total").innerHTML
+    document.getElementById("total").innerHTML = reqem.substring(0, input.length - 1);
+} 
+
+function kali() {
+    input = document.getElementById("total").innerHTML
+    input =  input + "*"
+    document.getElementById("total").innerHTML = input;
+}
+
+function bagi() {
+    input = document.getElementById("total").innerHTML
+    input =  input + "/"
+    document.getElementById("total").innerHTML = input;
+}
+function kurang() {
+    input = document.getElementById("total").innerHTML
+    input =  input + "-"
+    document.getElementById("total").innerHTML = input;
+
+}
+function tambah() {
+    input = document.getElementById("total").innerHTML
+    input =  input + "+"
+    document.getElementById("total").innerHTML = input;
+
+}
+
+
+
+function cek() {
+    if (isNaN(eval(hasil)) !== false) {
+        alert("input bukan berisi angka")
+    }
+}
+
+function hasil() {
+    hasil = document.getElementById("total").innerHTML
+    cek();
+    document.getElementById("total").innerHTML = eval(hasil)
+
+}
